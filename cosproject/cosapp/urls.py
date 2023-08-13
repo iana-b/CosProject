84ab6e66ca7dbbc7d125690b6e609dd3f0548ab1
iana-b <baraksina.ya@gmail.com>
2023-08-13 14:05:29 +0200

    add main page

--- cosproject/cosapp/urls.py
+++ cosproject/cosapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('product/new/', views.product_new, name='product_new'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('', views.product_list, name='product_list'),
 ]
