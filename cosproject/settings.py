c3242eb30b4126ac929b3bbf25bd862e14791af5
iana-b <baraksina.ya@gmail.com>
2023-09-21 15:08:48 +0200

    Remove vercel

--- cosproject/settings.py
+++ cosproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
