f5b0202e9c36ee5db643ad0ea7eb5d3014ffcb4a
Iana Baraksina <baraksina.ya@gmail.com>
2025-04-09 18:04:55 +0200

    added ordering for product form

--- cosapp/context_processors.py
+++ cosapp/context_processors.py
@@ -1,9 +1,10 @@
 from .models import Category, Brand
 
+
 def categories_processor(request):
     """
     Context processor to add categories and brands to all templates
     """
-    categories = Category.objects.order_by("title")
-    brands = Brand.objects.order_by("title")
-    return {'categories': categories, 'brands': brands} 
+    categories = Category.objects.all()
+    brands = Brand.objects.all()
+    return {'categories': categories, 'brands': brands}
--- cosapp/models.py
+++ cosapp/models.py
@@ -6,12 +6,18 @@ from django.contrib.auth.models import User
 class Brand(models.Model):
     title = models.CharField(max_length=30)
 
+    class Meta:
+        ordering = ["title"]
+
     def __str__(self):
         return self.title
 
 
 class Category(models.Model):
     title = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ["title"]
 
     def __str__(self):
         return self.title
